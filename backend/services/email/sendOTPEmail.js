b47c735c2c45f833497bb78009b65a93b9215cff
hezbon-dev <hezbonmagajoogwari@gmail.com>
2026-05-27 13:19:29 +0300

    Improved OTP UI and auto verification

--- backend/services/email/sendOTPEmail.js
+++ backend/services/email/sendOTPEmail.js
@@ -37,7 +37,7 @@ const sendOTPEmail = async (
           <h1>${otp}</h1>
 
           <p>
-            This code expires in 1 minute.
+            This code expires in 30 seconds.
           </p>
 
           <p>
